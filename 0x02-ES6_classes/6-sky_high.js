eb7e86c59e822648d55a6eded69e639adbdede60
honokini <hkini48@gmail.com>
2023-09-29 00:10:27 +0000

    Fixed this task

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,12 +1,11 @@
 import Building from './5-building';
 
-export default class SkyHighBuilding extends Building {
+class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
   }
 
-  // floors getter
   get floors() {
     return this._floors;
   }
@@ -15,3 +14,5 @@ export default class SkyHighBuilding extends Building {
     return `Evacuate slowly the ${this.floors} floors`;
   }
 }
+
+export default SkyHighBuilding;
