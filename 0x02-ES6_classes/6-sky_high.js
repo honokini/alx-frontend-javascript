3471f4a3e0fddf4983d8bcaad8a62be37a692d2b
honokini <hkini48@gmail.com>
2023-09-29 00:21:31 +0000

    Task 6 is Ok now

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,18 +1,28 @@
-import Building from './5-building';
+// Import the Building class from ./6-sky_high.js
+import Building from './6-sky_high.js';
 
+// Define the SkyHighBuilding class that extends Building
 class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    super(sqft);
+    super(sqft); // Call the constructor of the parent class (Building)
     this._floors = floors;
   }
 
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Getter for floors
   get floors() {
     return this._floors;
   }
 
+  // Override the evacuationWarningMessage method
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors`;
+    return `Evacuate slowly the ${this._floors} floors.`;
   }
 }
 
+// Export the SkyHighBuilding class
 export default SkyHighBuilding;
