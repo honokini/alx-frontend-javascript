0ae9455fa582098d0a53f83e38962589da1e255e
honokini <hkini48@gmail.com>
2023-09-29 01:04:08 +0000

    Let's move

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,21 +1,18 @@
-/*eslint-disable */
 import Building from './5-building';
 
-export default class SkyHighBuilding extends Building{
-    constructor (sqft, floors) {
-        super(sqft);
-        this._floors = floors;
-    }
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-    get sqft () {
-        return this._sqft;
-    }
+  get floors() {
+    return (this._floors);
+  }
 
-    get floors () {
-        return this._floors;
-    }
-
-    evacuationWarningMessage () {
-        return `Evacuate slowly the ${this._floors} floors`;
-    }
+  evacuationWarningMessage() {
+    return (`Evacuate slowly the ${this.floors} floors`);
+  }
 }
+
+export default SkyHighBuilding;
